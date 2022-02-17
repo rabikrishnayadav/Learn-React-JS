a90a64c96bb568ec5f6a402c49a4d26bfbac0ff3
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-17 20:32:29 +0545

    jsx explain

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-var React = require('react');
-var ReactDOM  = require('react-dom');
-
+import React from 'react'; // this is importing for use jsx in app
+import ReactDOM from 'react-dom'; // this is importing for use render method which will show output
 ReactDOM.render(<h1>Hello World!!</h1>,document.getElementById('root'));
+
+// jsx full name is javascript extension or javasxript XML
+// jsx is the html code its mean uses of html is called jsx
+
+// pure javascript
+var h1 = document.createElement('h1');
+h1.innerHTML = 'Pure Java Script';
+document.getElementById('root').appendChild(h1);
