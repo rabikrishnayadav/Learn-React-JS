40dafda97af292bfbc3d024ca7ba477a8dcc1928
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-19 10:04:11 +0545

    greeting message will change with time

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'; // this is importing for use render method whi
 
 import './index.css'; // importing class file for style
 
-ReactDOM.render(<h1>Hello World!!</h1>,document.getElementById('root'));
+// ReactDOM.render(<h1>Hello World!!</h1>,document.getElementById('root'));
 
 // jsx full name is javascript extension or javasxript XML
 // jsx is the html code its mean uses of html is called jsx
@@ -52,20 +52,20 @@ ReactDOM.render(<h1>Hello World!!</h1>,document.getElementById('root'));
 // );
 
 // 2) use <> </> bracket
-ReactDOM.render(
-<>
-<h1>The Netflix Pick</h1>
-<p>List of 5 Best Series.</p>
-<ol>
-  <li>Dark</li>
-  <li>Extra Curricular</li>
-  <li>My Holo Love</li>
-  <li>My first-2 Love</li>
-  <li>Mr Robot</li>
-</ol>
-</>,
-document.getElementById('root')
-);
+// ReactDOM.render(
+// <>
+// <h1>The Netflix Pick</h1>
+// <p>List of 5 Best Series.</p>
+// <ol>
+//   <li>Dark</li>
+//   <li>Extra Curricular</li>
+//   <li>My Holo Love</li>
+//   <li>My first-2 Love</li>
+//   <li>Mr Robot</li>
+// </ol>
+// </>,
+// document.getElementById('root')
+// );
 
 // jsx expression
 // here we use javascript inside jsx
@@ -110,28 +110,60 @@ document.getElementById('root')
 
 // JSX Attributes
 
-const Name = 'Rabi Kr Yadav';
-const photo = 'https://picsum.photos/215/200';
-const photo1 = 'https://picsum.photos/220/200';
-const mywebLink = 'https://rabikrishnayadav.com.np';
-const extcss = {
-  textAlign: 'center',
-  fontWeight: 'bold',
-  fontFamily: '"Gideon Roman", cursive'
+// const Name = 'Rabi Kr Yadav';
+// const photo = 'https://picsum.photos/215/200';
+// const photo1 = 'https://picsum.photos/220/200';
+// const mywebLink = 'https://rabikrishnayadav.com.np';
+// const extcss = {
+//   textAlign: 'center',
+//   fontWeight: 'bold',
+//   fontFamily: '"Gideon Roman", cursive'
+// }
+// ReactDOM.render(
+// <>
+// <h1 className="heading"> My Name is {Name} </h1>
+// <div className="img_div">
+//   <img src='https://picsum.photos/210/200' alt="Random Images" />
+// <img src={photo} alt="Random Images" />
+// <a href={mywebLink} target='_blank'><img src={photo1} alt="Random Images" /></a>
+// </div>
+// <div style={extcss}>This is Internal Css</div>
+// <div style={{
+//   color: '#fa9191',
+//   textAlign: 'center'
+// }}>This is Inline Css</div>
+// </>,
+// document.getElementById('root')
+// );
+
+
+const greeting = 'Hello Sir,';
+let msg = '';
+const cssStyle = { };
+let curTime = new Date();
+curTime = curTime.getHours();
+if(curTime >= 1 && curTime < 12){
+  msg = 'Good Morning';
+  cssStyle.color = 'green';
+}else if(curTime >= 12 && curTime < 19){
+  msg = 'Good Afternoon';
+  cssStyle.color = 'orange';
+}else{
+  msg = 'Good Night';
+  cssStyle.color = 'black';
 }
+
+const headstyle = {
+  textAlign: 'center',
+  backgroundColor: 'gray',
+  flex: 'center',
+  padding: '6px 10px'
+}
+
 ReactDOM.render(
-<>
-<h1 className="heading"> My Name is {Name} </h1>
-<div className="img_div">
-  <img src='https://picsum.photos/210/200' alt="Random Images" />
-<img src={photo} alt="Random Images" />
-<a href={mywebLink} target='_blank'><img src={photo1} alt="Random Images" /></a>
-</div>
-<div style={extcss}>This is Internal Css</div>
-<div style={{
-  color: '#fa9191',
-  textAlign: 'center'
-}}>This is Inline Css</div>
-</>,
-document.getElementById('root')
-);
+  <>
+  <div style={headstyle}>
+  <h1>{greeting} <span style={cssStyle}>{msg}</span></h1>
+  </div>
+  </>,
+  document.getElementById('root'));
