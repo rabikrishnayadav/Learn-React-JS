abe0b550853dfe02ee56e6c9e6fc7dfd1f423c99
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-21 12:15:08 +0545

    handling multiple input form state

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -2,33 +2,53 @@ import React, {useState} from "react";
 import './index.css';
 
 const Forms = () => {
-    const [name, setName] = useState('');
-    const [fullName, setFullName] = useState();
-    
-    const inputEvent = (event) => {
-        setName(event.target.value);
-    };
-    
-    const [password, setPassword] = useState('');
-    const [fullPassword, setFullPassword] = useState();
+    const [inputCurValue, setNewInputValue] = useState({
+        fullName: '',
+        email: '',
+        password: ''
+    });
 
-    const inputPasswordEvent = (event) => {
-        setPassword(event.target.value);
+    const inputEvent = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setNewInputValue((prevValue) =>{
+            if(name === 'fullName'){
+                return{
+                    fullName: value,
+                    email: prevValue.email,
+                    password: prevValue.password
+                };
+            }else if(name === 'email'){
+                return{
+                    fullName: prevValue.fullName,
+                    email: value,
+                    password: prevValue.password
+                };
+            }else if(name === 'password'){
+                return{
+                    fullName: prevValue.fullName,
+                    email: prevValue.email,
+                    password: value
+                };
+            }
+        });
     };
     
     const onSubmit = (event) => {
         event.preventDefault();
-        setFullName(name);
-        setFullPassword(password);
     }
 
     return(
         <>
         <form onSubmit={onSubmit}>
            <div className='eventDiv'>
-                <h1>Hello, {fullName} <br /> Your Password is: {fullPassword} </h1>
-               <input type="text" placeholder='Enter Your Name' value={name} onChange={inputEvent}/>
-               <input type="password" placeholder='Enter Your Password' value={password} onChange={inputPasswordEvent}/>
+                <h1>Hello, {inputCurValue.fullName} <br />
+                Email: {inputCurValue.email} <br />
+                Password: {inputCurValue.password} </h1>
+               <input type="text" placeholder='Enter Your Name' name="fullName" value={inputCurValue.fullName} onChange={inputEvent}/>
+               <input type="email" placeholder='Enter Your Email' name="email" value={inputCurValue.email} onChange={inputEvent}/>
+               <input type="password" placeholder='Enter Your Password' name="password" value={inputCurValue.password} onChange={inputEvent}/>
                <button type="submit"> Submit 👍</button>
            </div>
         </form>
