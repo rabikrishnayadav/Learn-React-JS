f53ad61545241f26fac3dd96697654dbb19ff1df
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-21 21:39:02 +0545

    material ui add

--- src/IncDecNum.jsx
+++ src/IncDecNum.jsx
@@ -1,5 +1,13 @@
 import React, {useState} from "react";
 import './index.css';
+import AddIcon from '@material-ui/icons/Add';                   // for v4
+import DeleteIcon from '@material-ui/icons/Delete';             // for v4
+import Button from '@material-ui/core/Button';                   // for v4
+import Tooltip from '@material-ui/core/Tooltip';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+// import Button from '@mui/material/Button';
+// import AddCircleIcon from '@mui/icons-material/AddCircle';       // for v5
+// import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
 const IncDecNum = () => {
 
@@ -26,8 +34,12 @@ const IncDecNum = () => {
                     <h1 style={{backgroundColor:'#30336b'}}> {num} </h1>
                     <br />
                     <div className="btn_div">
-                        <button onClick={incNum}>Increm</button>
-                        <button onClick={decNum}>Decrem</button>
+                        <Tooltip title="Add">
+                        <Button className='btn_green' onClick={incNum}><AddIcon /></Button>
+                        </Tooltip>
+                        <Tooltip title="Delete">
+                        <Button className='btn_red' onClick={decNum}><DeleteIcon /></Button>
+                        </Tooltip>
                     </div>
                 </div>
             </div>
