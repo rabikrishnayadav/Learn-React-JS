aee1ec2e23c9d6dadaeed7922c6d11dd00fcfaad
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-21 12:28:30 +0545

    one more input feild add

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -5,31 +5,41 @@ const Forms = () => {
     const [inputCurValue, setNewInputValue] = useState({
         fullName: '',
         email: '',
-        password: ''
+        password: '',
+        number: ''
     });
 
     const inputEvent = (event) => {
-        const value = event.target.value;
-        const name = event.target.name;
-
+        const {name, value} = event.target;
+        
         setNewInputValue((prevValue) =>{
             if(name === 'fullName'){
                 return{
                     fullName: value,
                     email: prevValue.email,
-                    password: prevValue.password
+                    password: prevValue.password,
+                    number: prevValue.number
                 };
             }else if(name === 'email'){
                 return{
                     fullName: prevValue.fullName,
                     email: value,
-                    password: prevValue.password
+                    password: prevValue.password,
+                    number: prevValue.number
                 };
             }else if(name === 'password'){
                 return{
                     fullName: prevValue.fullName,
                     email: prevValue.email,
-                    password: value
+                    password: value,
+                    number: prevValue.number
+                };
+            }else if(name === 'number'){
+                return{
+                    fullName: prevValue.fullName,
+                    email: prevValue.email,
+                    password: prevValue.password,
+                    number: value
                 };
             }
         });
@@ -45,9 +55,11 @@ const Forms = () => {
            <div className='eventDiv'>
                 <h1>Hello, {inputCurValue.fullName} <br />
                 Email: {inputCurValue.email} <br />
+                Number: {inputCurValue.number} <br />
                 Password: {inputCurValue.password} </h1>
                <input type="text" placeholder='Enter Your Name' name="fullName" value={inputCurValue.fullName} onChange={inputEvent}/>
                <input type="email" placeholder='Enter Your Email' name="email" value={inputCurValue.email} onChange={inputEvent}/>
+               <input type="text" placeholder='Enter Your Mobile Number' name="number" value={inputCurValue.number} onChange={inputEvent}/>
                <input type="password" placeholder='Enter Your Password' name="password" value={inputCurValue.password} onChange={inputEvent}/>
                <button type="submit"> Submit 👍</button>
            </div>
