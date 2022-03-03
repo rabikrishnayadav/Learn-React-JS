c07b779dbe408e6cbb4fea50f3e9d4bb27548385
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-03-03 08:31:15 +0545

    useLocation Hook

--- src/component/Router/User.jsx
+++ src/component/Router/User.jsx
@@ -1,13 +1,17 @@
+import { Button, ListItemSecondaryAction } from '@material-ui/core';
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useLocation} from 'react-router-dom';
 
 const User = () => {
 
     const {name} = useParams();
+    const location = useLocation();
   return (
     <>
     <h1>This is User Page</h1>
     <h2>The User Name is { name }</h2>
+    <h3>User Current Location is {location.pathname}</h3>
+    {location.pathname === `/user/rabi/yadav` ? <Button onClick={ () => alert('you are awesome')}>click me</Button> : null}
     </>
   );
 };
