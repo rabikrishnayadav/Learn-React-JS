cf7622b178cd8e03c9aa8f3cef0e8b1d4cf661b4
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-03-05 19:39:17 +0545

    Api call to get Pokemon Json data using Axios

--- src/component/Api/Covid/Pokemon.js
+++ src/component/Api/Covid/Pokemon.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 const Pokemon = () => {
 
-    const [num, setNum] = useState(0);
+    const [num, setNum] = useState();
+    const [name, setName] = useState();
+    const [moves, setMoves] = useState();
+
+    useEffect(() => {
+        async function getData(){
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            setName(res.data.name);
+            setMoves(res.data.moves.length);
+        }
+        getData();
+    });
+
   return (
     <>
-        <div className="container pt-5" style={{height:'100vh'}}>
-            <h1>You Choose {num} value</h1>
-            <select value={num} onChange={(event) => {
+        <div className="container pt-5 text-center" style={{height:'50vh'}}>
+            <h1>You Choose <span className='text-danger'> {num} value </span></h1>
+            <h1>The Name is <span className='text-danger text-capitalize'> {name} </span></h1>
+            <h1><span className='text-info text-capitalize'>{name} </span> have <span className='text-danger'> {moves} moves </span></h1>
+            <select style={{width:'100px', height:'30px',textAlign:'center'}} value={num} onChange={(event) => {
                 setNum(event.target.value);
             }}>
                 <option value="1">1</option>
